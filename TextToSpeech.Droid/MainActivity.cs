2240f88269a5cfb86f3f8c8b80afe01063220e1c
Jonathan Goldberger <github@johnnygold.com>
2014-09-10 15:43:39 -1000

    Test 2

--- TextToSpeech.Droid/MainActivity.cs
+++ TextToSpeech.Droid/MainActivity.cs
@@ -9,7 +9,6 @@ using Android.OS;
 using Android.Speech.Tts;
 using System.Collections.Generic;
 
-// Test Change
 namespace TextToSpeech.Droid
 {
     [Activity(Label = "TextToSpeech.Droid", MainLauncher = true, Icon = "@drawable/icon")]
